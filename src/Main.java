5c37a8910dc4e2d42c7a9d7eadff653d51f70724
khenlagusad <khenlagusad@gmail.com>
2025-11-17 14:53:00 +0800

    Initial commit for Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import model.User;
+import view.CreateAccountInterface;
+import view.LoginInterface;
+import view.admin.AdminInterface;
+import view.user.UserInterface;
+
 public class Main{
     public static void main (String[] args){
-
+        new CreateAccountInterface();
     }
 }
